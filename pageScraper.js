c89e7be9d50a7999d572005de10b56325d7a2ac9
shubham-ch247 <shubham.s@chapter247.com>
2022-05-17 11:13:29 +0530

    Use JSON.stringify in rate

--- pageScraper.js
+++ pageScraper.js
@@ -6,54 +6,37 @@ const scraperObject = {
     // Navigate to the selected page
     await page.goto(this.url);
     const data = [];
-    const rate = [];
     // Wait for the required DOM to be rendered
     await page.waitForSelector(".ul_rooms");
-    // Get the link to all the required books
     let urls = await page.$$(".ul_room");
-    let Obj = await page.$$(".ul_stayPrice");
     console.log(urls.length, "urls");
     for (let i = 0; i < urls.length; i++) {
       const roomTitle = await urls[i].$eval("h2", (text) =>
       text.textContent.replace(/(\r\n\t|\n|\r|\t)/gm, "").trim()
       );
-      const roomObj = {
-        roomTitle: roomTitle,
-        rate: rate,
-      };
-      data.push(roomObj);
+      await page.waitForSelector(".");
+      let Obj = await page.$$(".ul_stayPrice");
+      const rate = [];
       for (let j = 0; j < Obj.length; j++) {
         const roomPrice = await Obj[j].$eval(
           ".price",
           (number) => number.textContent
-          );
-          //rate object
-        let rateObj = {
+        );
+        //rate object
+        const rateObj = {
           roomPrice: parseInt(roomPrice),
         };
         //push price inside rate array
         rate.push(rateObj);
       }
+      const roomObj = {
+        roomTitle: roomTitle,
+        roomRate: rate,
+      };
+      data.push(roomObj);
     }
-    console.log(data);
-    // let newData = [];
-    // data.map((value) => {
-    //   const result = value.rate.filter(
-    //     (thing, index, self) =>
-    //       index ===
-    //       self.findIndex(
-    //         (t) => t.place === thing.place && t.roomPrice === thing.roomPrice
-    //       )
-    //   );
-    //   let pushobj = {
-    //     roomTitle: value.roomTitle,
-    //     Rate: result,
-    //   };
-    //   newData.push(pushobj);
-    // });
-    // newData.map((data) => {
-    //   console.log(data);
-    // });
+    //const hotel = JSON.stringify(data)
+    console.log(JSON.stringify(data));
   },
 };
 module.exports = scraperObject;
